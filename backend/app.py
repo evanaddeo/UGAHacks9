3070b69364745a15e1212ac44cd519f014be1a38
Evan Addeo <evanaddeo@gmail.com>
2024-02-09 22:19:52 -0500

    Created models & SQLAlchemy connection

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from models import db
+from models.user import User
+
 app = Flask(__name__)
+
+# Setup db info 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://devs:dev_pass@localhost/hacksproject'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize alchemy tool
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
@@ -7,3 +18,14 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+# Notes for queries
+#
+# Retrieve all users w/ {   all_users = User.query.all()   }
+# Retrieve a user by id w/ {   user_by_id = User.query.get(1)   }
+# filtered query w/ {   filtered_users = User.query.filter_by(username='whateveer-name').all()   }
+# Create a new user w/ {    new_user = User(username='evan_a, email='evan@addeo.com')
+#                           db.session.add(new_user)
+#                           db.session.commit()    }
