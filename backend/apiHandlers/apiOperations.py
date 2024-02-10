d625fc3c447199674583553931ee115dd986a7f4
Evan Addeo <evanaddeo@gmail.com>
2024-02-10 13:32:12 -0500

    Updated for intolerances in user objects & db

--- backend/apiHandlers/apiOperations.py
+++ backend/apiHandlers/apiOperations.py
@@ -5,7 +5,7 @@ import requests
 # extendedIngredients : a list that contains all the ingredients req by recipe
 # analyzedInstructions : a list that contains cooking instructions
 # nutrition : an object that contains detailed nutrition info ab recipe
-def get_recipe_details(recipe_id):
+def get_recipe_details(recipe_id, intolerances):
     url = "https://api.spoonacular.com/recipes/{}/information".format(recipe_id)
     params = {
         "apiKey": "a581f0a1ab1043379b3c2bc3a853d918",
@@ -18,12 +18,15 @@ def get_recipe_details(recipe_id):
 # Include "breakfast", "lunch", or "dinner" as input
 #
 # Returns 30 options for that particular kind of meal
-def get_meal_recipes(meal_type):
+#
+# intolerances : Listed as "None" || "Peanut" || "Peanut,Seafood" ... etc
+def get_meal_recipes(meal_type, intolerances):
     url = "https://api.spoonacular.com/recipes/complexSearch"
     params = {
         "apiKey": "a581f0a1ab1043379b3c2bc3a853d918",
         "type": meal_type,
         "number": 30,
+        "intolerances": intolerances,
         "instructionsRequired": True
     }
     response = requests.get(url, params=params)
