7f45fd63398a71d19fd6a87e0ba42544fd130d21
Evan Addeo <evanaddeo@gmail.com>
2024-02-10 15:25:36 -0500

    Updated navbar looks

--- frontend/perfect-plate/src/app/page.js
+++ frontend/perfect-plate/src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import PPLogo from '../../images/PP_logo2.png';
+import ProfilePic from '../../images/profile.png';
 
 export default function Home() {
   return (
@@ -8,11 +9,15 @@ export default function Home() {
       <div className={styles.description}>
         <nav>
           <ul>
+          <Image className={styles.logo} src={PPLogo} alt="PerfectPlate Logo" />
+          <h3>PerfectPlate</h3>
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#weekly-glance">Weekly Glance</a></li>
           <li><a href="#recap">Recap</a></li>
+          <Image className={styles.prof} src={ProfilePic} alt="Profile Logo" />
         </ul>
+        
       </nav>
         <div>
           <a
