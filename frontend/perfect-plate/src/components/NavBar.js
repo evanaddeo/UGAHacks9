3b23828dc135349a1babe7362f6208392807a85c
kaitlynback13 <klb24634@uga.edu>
2024-02-10 19:16:59 -0500

    About page

--- frontend/perfect-plate/src/components/NavBar.js
+++ frontend/perfect-plate/src/components/NavBar.js
@@ -12,10 +12,10 @@ export default function NavBar() {
             <ul>
             <Image className={styles.logo} src={PPLogo} alt="PerfectPlate Logo" />
             <h3>PerfectPlate</h3>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#weekly-glance">Weekly Glance</a></li>
-            <li><a href="#recap">Recap</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/weekly">Weekly Glance</a></li>
+            <li><a href="/recap">Recap</a></li>
             <Link href="/login">
             <Image className={styles.prof} src={ProfilePic} alt="Profile Logo" />
             </Link>
